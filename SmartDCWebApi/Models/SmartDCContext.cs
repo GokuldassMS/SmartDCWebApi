9ac8c55457ebce516f03ea23167042dd6aee3540
GokuldassMS <55793777+GokuldassMS@users.noreply.github.com>
2021-09-02 21:19:52 +0530

    Initial Commit

---
+++ SmartDCWebApi/Models/SmartDCContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartDCWebApi.Models
+{
+    public partial class SmartDCContext : DbContext
+    {
+        public SmartDCContext(DbContextOptions<SmartDCContext> options)
+           : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
